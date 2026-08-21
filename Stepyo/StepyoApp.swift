8405c33129d486b8effb938e7a84c288c686af4d
Ilmir Rakhmangulov <hhugz050@gmail.com>
2026-08-21 17:29:59 +0500

    1st

---
+++ Stepyo/StepyoApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepyoApp.swift
+//  Stepyo
+//
+//  Created by Charlie Hebdo on 8/21/26.
+//
+
+import SwiftUI
+
+@main
+struct StepyoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
